bd398e82fde3cddbe026ac07398fc2308da9edbe
Katerina <kat6123@yandex.ru>
2018-05-15 07:45:51 +0300

    Restructure package

---
+++ lcs/file.go
@@ -0,0 +1,24 @@
+package lcs
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func ReadFile(path string) ([]string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error while reading %s file: %v\n", path, err)
+		// Do I need to forward error?
+		return nil, err
+	}
+	return strings.Split(string(data), "\n"), err
+}
+
+func Print(file []string) {
+	for _, s := range file {
+		fmt.Println(s)
+	}
+}
